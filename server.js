1341b0cdd3c3d1c8212edd5479e01931bb6d7aa9
lackingSemicolon <43654181+lackingSemicolon@users.noreply.github.com>
2020-03-15 15:26:41 -0700

    add first GET route

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express'); // Let us use the library
 const app = express(); // instanciate the library
 
+
+const mockUserDate = [
+    {name: 'Tam'},
+    {name: 'Nguyen'}
+]
+
+app.get('/users', function(req,res){
+    res.json({
+         success: true,
+         message: 'successfully got users. Nice!',
+         users: mockUserData
+    })
+})
+
 app.listen(800, function() {
     console.log("server is running")
 })
-
