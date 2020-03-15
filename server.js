b49009defc5b4b5666f774ed86cf7118e1ca40d3
lackingSemicolon <43654181+lackingSemicolon@users.noreply.github.com>
2020-03-15 12:04:10 -0700

    set up express server

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express'); // Let us use the library
+const app = express(); // instanciate the library
+
+app.listen(800, function() {
+    console.log("server is running")
+})
+
