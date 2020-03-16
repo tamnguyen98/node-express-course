d20c933eaf7cd5c1cefe94150311f94f11a904d9
lackingSemicolon <43654181+lackingSemicolon@users.noreply.github.com>
2020-03-15 18:38:24 -0700

    add dynamic GET route
    Getting variables from url

--- server.js
+++ server.js
@@ -2,19 +2,29 @@ const express = require('express'); // Let us use the library
 const app = express(); // instanciate the library
 
 
-const mockUserDate = [
+const mockUserData = [
     {name: 'Tam'},
     {name: 'Nguyen'}
 ]
 
-app.get('/users', function(req,res){
-    res.json({
-         success: true,
-         message: 'successfully got users. Nice!',
-         users: mockUserData
-    })
+app.get('/users/:id',function(req,res){ // In Express, words with a colon in front of them in the url are treated as variables.
+	console.log(req.params.id) // print the variable to console
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id // accessing the variable
+	})
 })
 
-app.listen(800, function() {
+// Basic get request
+// app.get('/users', function(req,res){
+//     res.json({
+//          success: true,
+//          message: 'successfully got users. Nice!',
+//          users: mockUserData
+//     })
+// })
+
+app.listen(8000, function() {
     console.log("server is running")
 })
